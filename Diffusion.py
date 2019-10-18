0451ee1a9a6d46bf99c12b16be4e644f1dab73ad
Henry Manthorpe <henrymanthorpe@gmail.com>
2019-10-18 17:25:33 +0100

    Cleaned up deprectrated test files, Diffusion is now the main linear diffusion script -Henry

--- Diffusion.py
+++ Diffusion.py
@@ -8,8 +8,6 @@ import numpy as np
 from math import sqrt, ceil
 import datetime
 import os
-import time
-
 
 def buildBinomial(n,t):
     return np.random.binomial(t, 0.5, (n,3))
@@ -135,12 +133,56 @@ def linearDiffusionSave(results):
     np.savetxt(fileName, results, delimiter = '\t')
 # saves output to text file in work directory.
 
-def chunkedDiffusion(m, T, dc, rt, ts):
-    nMax = 1000
+def chunkedDiffusionBuild(m, T, dc, rt, ts, tChunk):
     dv = diffusionVars(m, T, dc)
     tv = timeVars(dv, rt, ts)
-    chunks = ceil(tv[0]/nMax)
-    return chunks
+    chunks = ceil(rt/tChunk)
+    chunkN = ceil(tChunk/ts)
+    seed = np.zeros((chunks, 1), dtype=int)
+    results = np.zeros((chunks, 4))
+    for x in range(chunks):
+        seed[x] = np.random.randint(2**31-1)
+        np.random.seed(seed[x])
+        k = linearDiffusion(chunkN, tv[1], dv[2], tv[2])
+        results[x] = np.sum(k, axis=0)
+    output = np.hstack((seed, results))  
+    return output, [dv, tv, m, T, dc, rt, ts, tChunk, chunkN]
+
+def chunkedDiffusionRender(k, tStart, tEnd):
+    data = k[0]
+    var = k[1]
+    dv = var[0]
+    tv = var[1]
+    chunkN=int(var[8])
+    del k, var
+    x = 0
+    seeds,position,time=np.split(data, [1,4], axis=1)
+    del data
+    while True:
+        if np.sum(time[:x], axis=0) > tStart:
+            iStart = x-1
+            print(iStart)
+            break
+        x=x+1
+    while True:
+        if np.sum(time[:x], axis=0) > tEnd:
+            iEnd = x
+            print(iEnd)
+            break
+        x=x+1
+    posStart = np.sum(position[:iStart], axis=0)
+    timeStart = np.sum(time[:iStart], axis=0)
+    results=np.hstack((posStart, timeStart))
+    for i in range(iStart, iEnd):
+        np.random.seed(int(seeds[i]))
+        p = linearDiffusion(chunkN, tv[1], dv[2], tv[2])
+        results = np.vstack((results, p))
+    return results
+
+        
+        
+        
+        
 
     
     
--- RotationalDiffusion.py
+++
@@ -1,7 +0,0 @@
-
-
-def SimpleRotation(frictionalDragCoefficent, T):
-    particleHeading = [0,0,0,0] #x,y,z,t
-    
-    
-    
