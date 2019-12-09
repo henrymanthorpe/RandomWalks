2edb4fe5914f92719aa65ce5de1d9327d877e315
Henry Manthorpe <henrymanthorpe@gmail.com>
2019-12-09 00:05:04 +0000

    Finished work on MSD... finally. Extended diffusion to rotational as well, produces array of cartesian unit vectors that form a sphere upon graphing points.

--- ClassLibrary.py
+++ ClassLibrary.py
@@ -8,6 +8,8 @@ Created on Thu Nov  7 21:38:37 2019
 import numpy as np
 from scipy import constants
 from math import sqrt, ceil
+import os
+import datetime
 
 pi = np.pi
 boltz = constants.Boltzmann
@@ -15,28 +17,58 @@ avo = constants.Avogadro
 g = constants.g
 
 class RunVars:
+    # Holds User-defined Variables for Particle Simulations
     
     def __init__(self):
         self.absolute_viscosity=0
-        self.base_time=0
+        self.temperature=0
         self.external_forces=(0,0,0)
-        self.intermediate_time=0
         self.molecular_mass=0
         self.radius=0
         self.run_time=0
-        self.temperature=0
+        self.base_time=0
         
-    def Define(self, molecular_mass, temperature, absolute_viscosity, radius, external_forces, run_time, intermediate_time, base_time):
-        self.absolute_viscosity=absolute_viscosity
-        self.base_time=base_time
-        self.external_forces=external_forces
-        self.intermediate_time=intermediate_time
-        self.molecular_mass=molecular_mass
-        self.radius=radius
-        self.run_time=run_time
-        self.temperature=temperature
-    
+
     def Build(self):
+        
+        self.diffusion_constant_rotational = np.zeros(3)
+        if type(self.radius) == float :
+            self.frictional_drag_linear = 6*pi*self.absolute_viscosity*self.radius
+            self.frictional_drag_rotational = 8*pi*self.absolute_viscosity*(self.radius**3)
+            self.diffusion_constant_rotational[0] = self.temperature*boltz/self.frictional_drag_rotational
+            self.diffusion_constant_rotational[1] = self.temperature*boltz/self.frictional_drag_rotational
+            self.diffusion_constant_rotational[2] = self.temperature*boltz/self.frictional_drag_rotational
+        elif type(self.radius) == list :
+            q = np.log(2*self.radius[0]/self.radius[1])
+            self.frictional_drag_linear = 6*pi*self.absolute_viscosity*self.radius[0]/q
+            self.frictional_drag_rotational_minor = (8*pi*self.absolute_viscosity*(self.radius[0]**3)/3)/(q+0.5)
+            self.frictional_drag_rotational_major = (16/3)*pi*self.absolute_viscosity*self.radius[0]*self.radius[1]**2
+            self.diffusion_constant_rotational[0] = self.temperature*boltz/self.frictional_drag_rotational_major
+            self.diffusion_constant_rotational[1] = self.temperature*boltz/self.frictional_drag_rotational_minor
+            self.diffusion_constant_rotational[2] = self.temperature*boltz/self.frictional_drag_rotational_minor
+        else:
+            print("Error: Invalid Datatype for radius")
+            return 0
+        
+        self.diffusion_constant_linear = self.temperature*boltz/self.frictional_drag_linear
+        self.particle_mass = self.molecular_mass/avo
+        self.rms_velocity = sqrt(boltz*self.temperature/self.particle_mass)
+        self.step_linear = 2*self.diffusion_constant_linear/self.rms_velocity
+        self.step_rate_linear = self.rms_velocity/self.step_linear
+        self.step_time_linear = 1.0/self.step_rate_linear
+        self.external_forces = self.external_forces.reshape(3,1)
+        self.external_acceleration = self.external_forces/self.particle_mass
+        self.external_force_step = self.external_acceleration*0.5*self.step_time_linear**2
+        self.sample_total = ceil(self.run_time/self.base_time)
+        self.run_time = self.base_time*self.sample_total #Increases run_time to include integer number of samples
+        self.sample_steps_linear = self.step_rate_linear*self.base_time
+        self.step_rotational = [2*self.diffusion_constant_rotational[x]/self.rms_velocity for x in range(3)]
+        self.step_rate_rotational = [self.rms_velocity/self.step_rotational[x] for x in range(3)]
+        self.step_time_rotational = [1.0/self.step_rate_rotational[x] for x in range(3)]
+        self.sample_steps_rotational = [self.step_rate_rotational[x]*self.base_time for x in range(3)]
+        
+     
+    def Input(self):
         print("Welcome to the guided Variable builder \n")
         print("--- Particle Variables ---")
         while True:
@@ -48,6 +80,8 @@ class RunVars:
                     print("Error: Negative Mass not possible")
             except ValueError:
                 print("Error: That is not a numerical input")
+        
+        
         
         while True:
             try:
@@ -133,15 +167,6 @@ class RunVars:
                 print("Error: That is not a numerical input")
         while True:
             try:
-                self.intermediate_time = float(input("Input intermediate time (s) : "))
-                if self.intermediate_time > 0:
-                    break
-                else:
-                    print("Error: Negative value not possible")
-            except ValueError:
-                print("Error: That is not a numerical input")
-        while True:
-            try:
                 self.base_time = float(input("Input base time (s) : "))
                 if self.base_time > 0:
                     break
@@ -149,47 +174,33 @@ class RunVars:
                     print("Error: Negative value not possible")
             except ValueError:
                 print("Error: That is not a numerical input")
-
-class DiffVars:
-    def __init__(self, v): #v is a RunVars object
-        self.diffusion_constant_rotational = np.zeros(3)
-        if type(v.radius) == float :
-            self.frictional_drag_linear = 6*pi*v.absolute_viscosity*v.radius
-            self.frictional_drag_rotational = 8*pi*v.absolute_viscosity*(v.radius**3)
-            self.diffusion_constant_rotational[0] = v.temperature*boltz/self.frictional_drag_rotational
-            self.diffusion_constant_rotational[1] = v.temperature*boltz/self.frictional_drag_rotational
-            self.diffusion_constant_rotational[2] = v.temperature*boltz/self.frictional_drag_rotational
-        elif type(v.radius) == list :
-            q = np.log(2*v.radius[0]/v.radius[1])
-            self.frictional_drag_linear = 6*pi*v.absolute_viscosity*v.radius[0]/q
-            self.frictional_drag_rotational_minor = (8*pi*v.absolute_viscosity*(v.radius[0]**3)/3)/(q+0.5)
-            self.frictional_drag_rotational_major = (16/3)*pi*v.absolute_viscosity*v.radius[0]*v.radius[1]**2
-            self.diffusion_constant_rotational[0] = v.temperature*boltz/self.frictional_drag_rotational_major
-            self.diffusion_constant_rotational[1] = v.temperature*boltz/self.frictional_drag_rotational_minor
-            self.diffusion_constant_rotational[2] = v.temperature*boltz/self.frictional_drag_rotational_minor
-        else:
-            print("Error: Invalid Datatype for radius")
-            return 0
         
-        self.diffusion_constant_linear = v.temperature*boltz/self.frictional_drag_linear
-        self.particle_mass = v.molecular_mass/avo
-        self.rms_velocity = sqrt(boltz*v.temperature/self.particle_mass)
-        self.step_linear = 2*self.diffusion_constant_linear/self.rms_velocity
-        self.step_rate = self.rms_velocity/self.step_linear
-        self.step_time = 1.0/self.step_rate
-        self.step_rotational = np.sqrt(2*self.step_time*self.diffusion_constant_rotational)
-        self.external_acceleration = v.external_forces/self.particle_mass
-        self.external_force_step = self.external_acceleration*0.5*self.step_time**2
-        self.sample_total = ceil(v.run_time/v.base_time)
-        self.sample_steps = self.step_rate*v.base_time
-        self.sample_time = self.step_time*self.sample_steps
-        self.intermediate_total = ceil(v.run_time/v.intermediate_time)
-        self.intermediate_sample = ceil(v.intermediate_time/v.base_time)
+        self.Build()
+    
+    def Save(self, f_name = ''):
+        if f_name == '':
+            i = 0
+            while os.path.exists("RunVariables "+str(datetime.date.today())+" "+str(i)+".npy"):
+                i = i+1
+        
+            f_name = "RunVariables "+str(datetime.date.today())+" "+str(i)
+        elif type(f_name) != str:
+            print("Error: File name must be a string")
+            return 1
+        out_list = [self.base_time,self.run_time,self.absolute_viscosity,self.temperature,self.external_forces,self.molecular_mass, self.radius]
+        out_array = np.array(out_list)
+        np.save(f_name, out_array)
+        return 0
+    
+    def Load(self, f_name):
+        in_array = np.load(f_name, allow_pickle=True)
+        self.base_time=in_array[0]
+        self.run_time=in_array[1]
+        self.absolute_viscosity=in_array[2]
+        self.temperature=in_array[3]
+        self.external_forces=in_array[4]
+        self.molecular_mass=in_array[5]
+        self.radius=in_array[6]
+        self.Build()
 
-class Particle:
-    def __init__(self, v):
-        self.linear_dis = np.zeros((v.intermediate_total,3))
-        self.rotational_dis = [x for x in range(v.intermediate_total)]
-        self.unit_vector = np.zeros((v.intermediate_total,3))
-        self.seed = np.zeros((v.intermediate_total,1),dtype=int)
-        self.run = 0
+        
